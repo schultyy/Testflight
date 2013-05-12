7956ad9653559c25813c3d6bcf5527223a2ab787
Jan Schulte <janschulte90@googlemail.com>
2013-05-12 18:12:55 +0200

    can show basic information about build

--- Testflight.Web/Scripts/BuildReport.js
+++ Testflight.Web/Scripts/BuildReport.js
@@ -1,3 +1,36 @@
 ﻿$(document).ready(function () {
-    var elems = $("#report-list > a");
+    $("#report-list > a").click(function (e) {
+        var that = this;
+        $.ajax({
+            type: "POST",
+            url: '/Configuration/ReportDetails',
+            dataType: 'json',
+            data: { reportId: $(this).attr("id") },
+            cache: false,
+            success: function (data) {
+
+                $("#result-details").empty();
+
+                var heading = $("<p>").attr("id", "heading")
+                        .text("Report");
+                $("#result-details").append(heading);
+
+                var list = $("<ul>");
+
+                for (var key in data.LogEntries) {
+                    var listEntry = $("<li>");
+                    var currentLogEntry = data.LogEntries[key][0];
+                    var message = currentLogEntry.Message;
+
+                    listEntry.append($("<p>").attr("class", "logentry-heading")
+                        .text(key));
+                    listEntry.append($("<p>").attr("class", "logentry-content")
+                        .text(message));
+                    list.append(listEntry);
+                }
+                $("#result-details").append(list);
+            }
+        });
+        e.preventDefault();
+    });
 });
