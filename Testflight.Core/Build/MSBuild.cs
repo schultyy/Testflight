2cd8fa282beac83bfe92154e4a0bb04f06bf6924
Jan Schulte <janschulte90@googlemail.com>
2013-05-12 15:39:18 +0200

    use msbuild capabilities from framework instead of calling the msbuild executable

--- Testflight.Core/Build/MSBuild.cs
+++ Testflight.Core/Build/MSBuild.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Execution;
 using TestFlight.Shared;
 
 namespace Testflight.Core
@@ -18,33 +20,51 @@ namespace Testflight.Core
 
         public IBuildResult Call(string solutionFile, BuildConfiguration buildConfiguration)
         {
-            if (string.IsNullOrEmpty(solutionFile))
-                throw new ArgumentNullException("solutionFile");
+            //string projectFileName = @"...\ConsoleApplication3\ConsoleApplication3.sln";
+            //ProjectCollection pc = new ProjectCollection();
+            //Dictionary<string, string> GlobalProperty = new Dictionary<string, string>();
+            //GlobalProperty.Add("Configuration", "Debug");
+            //GlobalProperty.Add("Platform", "x86");
 
-            var results = new BuildResult();
+            //BuildRequestData BuidlRequest = new BuildRequestData(projectFileName, GlobalProperty, null, new string[] { "Build" }, null);
 
-            var arguments = String.Join(" ", solutionFile, string.Format("/p:Configuration={0}", buildConfiguration));
+            //BuildResult buildResult = BuildManager.DefaultBuildManager.Build(new BuildParameters(pc), BuidlRequest);
 
-            var process = new Process()
-                              {
-                                  StartInfo = new ProcessStartInfo
-                                                  {
-                                                      WindowStyle = ProcessWindowStyle.Hidden,
-                                                      CreateNoWindow = true,
-                                                      FileName = Path,
-                                                      Arguments = arguments,
-                                                      UseShellExecute = false,
-                                                      RedirectStandardError = true,
-                                                      RedirectStandardOutput = true
-                                                  }
-                              };
+            var projectCollection = new ProjectCollection();
+            var properties = new Dictionary<string, string> {{"Configuration", buildConfiguration.ToString()}};
 
-            process.Start();
+            var buildRequest = new BuildRequestData(solutionFile, properties, null, new[] { "Build" }, null);
 
-            results.StdError = process.StandardError.ReadToEnd();
-            results.StdOut = process.StandardOutput.ReadToEnd();
-            results.ExitCode = process.ExitCode;
-            return results;
+            var result = BuildManager.DefaultBuildManager.Build(new BuildParameters(projectCollection), buildRequest);
+
+            return null;
+
+            //if (string.IsNullOrEmpty(solutionFile))
+            //    throw new ArgumentNullException("solutionFile");
+
+            //var results = new BuildResult();
+
+            //var arguments = String.Join(" ", solutionFile, string.Format("/p:Configuration={0}", buildConfiguration));
+
+            //var process = new Process()
+            //                  {
+            //                      StartInfo = new ProcessStartInfo
+            //                                      {
+            //                                          //WindowStyle = ProcessWindowStyle.Hidden,
+            //                                          //CreateNoWindow = true,
+            //                                          FileName = Path,
+            //                                          Arguments = arguments,
+            //                                          UseShellExecute = false,
+            //                                          RedirectStandardError = true,
+            //                                          RedirectStandardOutput = true
+            //                                      }
+            //                  };
+
+            //process.Start();
+            //results.StdError = process.StandardError.ReadToEnd();
+            //results.StdOut = process.StandardOutput.ReadToEnd();
+            //results.ExitCode = process.ExitCode;
+            //return results;
         }
     }
 }
