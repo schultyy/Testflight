d3e9026bd890d78f198a39f8ef71952663e054a9
Jan Schulte <janschulte90@googlemail.com>
2013-05-12 16:59:31 +0200

    added missing file

---
+++ Testflight.Core/Build/TargetResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Testflight.Core.Build
+{
+    public class TargetResult : ITargetResult
+    {
+        public Exception Exception { get; set; }
+        public ResultCode Result { get; set; }
+        public string Component { get; set; }
+    }
+}
